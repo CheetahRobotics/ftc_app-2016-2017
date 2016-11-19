f4fc5130ca1b629c04a8e8329a2a9578dc726956
Robert Woodley <rlwoodley@gmail.com>
2016-11-19 11:54:37 -0600

    Better names for Simple Tele Drive Op Mode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleDriveOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleTeleDriveOpMode.java
@@ -51,9 +51,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="My First OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@TeleOp(name="Simple 2 Motor OpMode", group="Linear Opmode")  // @Autonomous(...) is the other common choice
 //@Disabled
-public class MyFirstOpMode extends LinearOpMode {
+public class SimpleTeleDriveOpMode extends LinearOpMode {
 
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
