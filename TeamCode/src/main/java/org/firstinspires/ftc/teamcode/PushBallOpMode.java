1b543a213fe555b8807092fc433f2495a0f51a5c
Robert Woodley <rlwoodley@gmail.com>
2017-01-24 17:16:06 -0600

    adjust speed.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBallOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PushBallOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
@@ -13,6 +14,9 @@ public class PushBallOpMode extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
 
 
+    DcMotor leftMotor = null;
+    DcMotor rightMotor = null;
+
     static final double     FORWARD_SPEED = 0.6;
     static final double     TURN_SPEED    = 0.5;
 
@@ -23,22 +27,24 @@ public class PushBallOpMode extends LinearOpMode{
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
          */
-        robot.init(hardwareMap);
+//        robot.init(hardwareMap);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
         telemetry.update();
 
+        leftMotor  = hardwareMap.dcMotor.get("left_drive");
+        rightMotor = hardwareMap.dcMotor.get("right_drive");
           // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // Step through each leg of the path, ensuring that the Auto mode has not been stopped along the way
 
         // Step 1:  Drive forward for 3 seconds
-        robot.leftMotor.setPower(FORWARD_SPEED);
-        robot.rightMotor.setPower(FORWARD_SPEED);
+        leftMotor.setPower(FORWARD_SPEED /3.0);
+        rightMotor.setPower(-FORWARD_SPEED / 3.0);
         runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+        while (opModeIsActive() && (runtime.seconds() < 1.3)); {
             telemetry.addData("Path", "Leg 1: %2.5f S Elapsed", runtime.seconds());
             telemetry.update();
         }
